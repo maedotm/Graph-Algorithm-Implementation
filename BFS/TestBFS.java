4b0f49b437acc399ca8fd1013f8e11347d1aef6e
letkzelalem <letkzelalem98@gmial.com>
2022-06-15 16:35:36 +0300

    Breadth first search implementation

---
+++ BFS/TestBFS.java
@@ -0,0 +1,16 @@
+class TestBFS{
+	public static void main(String args[]){
+		Graph graph = new Graph(4);
+
+		graph.addEdge(0, 1);
+		graph.addEdge(0, 2);
+		graph.addEdge(1, 2);
+		graph.addEdge(2, 0);
+		graph.addEdge(2, 3);
+		graph.addEdge(3, 3);
+
+		System.out.println("Following is Breadth First Traversal "+"(starting from vertex 2)");
+
+		graph.BFS(2);
+	}
+}
